b5d1bcd5cfb5433fc29b29ab4886e177718dc04a
midhunkollillath <108817703+midhunkollillath@users.noreply.github.com>
2024-11-26 20:08:00 +0530

    main

--- frontend/src/constant/DefaultValues.js
+++ frontend/src/constant/DefaultValues.js
@@ -1,2 +1,6 @@
 
-export   const REST_API = 'http://localhost:4000'
+// export   const REST_API = 'http://localhost:4000'
+
+
+//live 
+export   const REST_API = 'https://wordcount-tracker-production.up.railway.app'
