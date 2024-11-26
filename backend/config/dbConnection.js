aec4e91391d417e4fed56c0e4d6d07742f1bc8af
midhunkollillath <108817703+midhunkollillath@users.noreply.github.com>
2024-11-26 19:47:50 +0530

    main

--- backend/config/dbConnection.js
+++ backend/config/dbConnection.js
@@ -1,15 +1,22 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME, 
-  process.env.DB_USER, 
-  process.env.DB_PASSWORD, 
-  {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-    port: process.env.DB_PORT || 3306,
-  }
-);
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME, 
+//   process.env.DB_USER, 
+//   process.env.DB_PASSWORD, 
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: process.env.DB_DIALECT,
+//     port: process.env.DB_PORT || 3306,
+//   }
+// );
+//live
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'mysql', 
+  dialectOptions: {
+    connectTimeout: 10000,
+  },
+});
 
 module.exports = sequelize;
